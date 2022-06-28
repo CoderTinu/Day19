6c9829fcd87507d690de8755d01a157dbcc21b66
Bhagyashri <bhagyashrijulme70@gmail.com>
2022-06-28 15:29:45 +0530

    Day 19 assignment

---
+++ UserRegistration.java
@@ -0,0 +1,42 @@
+package Day19;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	private static String FirstNamepattern ="^[A-Z][a-z]{3,}$" ;
+	private static String LastNamepattern = "^[A-Z][a-z]{3,}$";
+	
+	public void FirstnameValidation(String FirstNamepattern, String name) {
+		Pattern p = Pattern.compile(FirstNamepattern);
+
+		Matcher m = p.matcher(name);
+		boolean result = m.matches();
+		System.out.println(result);
+	}
+	public void LastnameValidation(String LastNamepattern,String name) {
+		Pattern p = Pattern.compile(LastNamepattern);
+		
+		Matcher m = p.matcher(name);
+		boolean result = m.matches();
+		System.out.println(result);
+	}
+
+	public static void main(String[] args) {
+		UserRegistration User = new UserRegistration();
+		
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("UserRegistration");
+		
+		System.out.println("Enter first name: ");
+		System.out.println("Enter Last name: ");
+		
+		String name = sc.next();
+	User.FirstnameValidation(FirstNamepattern,name);
+	
+	User.LastnameValidation(LastNamepattern,name);
+	}
+
+}
---
+++ UserRegistrationMain.java
@@ -0,0 +1,26 @@
+package Day19;
+
+import java.util.Scanner;
+
+public class UserRegistrationMain {
+public static void main(String[] args) {
+		
+
+        UserRegistration1 registration = new UserRegistration1();
+        Scanner scanner = new Scanner(System.in);
+        
+	 System.out.println("Enter Email: ");
+     String email = scanner.next();
+     registration.validateEmail(email);
+
+     System.out.println("Enter Phone-Number");
+     String number = scanner.next();
+     registration.validatePhoneNumber(number);
+
+     System.out.println("Enter Password: ");
+     String password = scanner.next();
+     registration.validatePassword(password);
+     registration.register();
+
+	}
+}
